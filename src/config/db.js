6e15a7aa72706c922554f0bf312832e17ff8ac4b
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-15 18:06:34 +0100

    chore

--- src/config/db.js
+++ src/config/db.js
@@ -1,22 +1,22 @@
-const { Sequelize } = require('sequelize');
-const config = require('../../db/config/config');
+import { Sequelize } from 'sequelize';
+import config from '../../db/config/config';
+
 require('dotenv').config();
 
 const { NODE_ENV } = process.env;
 
 const db = config[NODE_ENV];
 
-let sequelize;
-
-if (NODE_ENV === 'production') {
-  sequelize = new Sequelize(db.url, {
-    dialect: db.dialect,
-  });
-} else {
-  sequelize = new Sequelize(db.database, db.username, db.password, {
+const init = () => {
+  if (NODE_ENV === 'production') {
+    return new Sequelize(db.url, {
+      dialect: db.dialect,
+    });
+  }
+  return new Sequelize(db.database, db.username, db.password, {
     dialect: db.dialect,
     host: db.host,
   });
-}
+};
 
-module.exports = sequelize;
+export default init();
