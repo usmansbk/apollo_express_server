c9cecc0f59c0be5aec2c143494cbec62dfc5bf7e
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-16 14:52:44 +0100

    add db to context

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -1,10 +1,12 @@
 import typeDefs from './types';
 import resolvers from './resolvers';
+import db from '../../db/models';
 
 export default {
   typeDefs,
   resolvers,
   context: ({ req }) => ({
     me: req.headers?.authorization || 'Usman',
+    db,
   }),
 };
