8759d440609e8d8644268c59890cd5b6607706dd
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-16 12:08:41 +0100

    decouple

--- src/bin/www.js
+++ src/bin/www.js
@@ -1,20 +1,19 @@
 #! /usr/bin/env node
 import { ApolloServer } from 'apollo-server-express';
-import sequelize from '../config/db';
+import db from '../config/db';
 import app from '../server';
 import logger from '../config/logger';
 import schema from '../graphql';
 
 require('dotenv').config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 const server = new ApolloServer(schema);
 
 app.listen(PORT, async () => {
   logger.log(`🚀 Serving at http://localhost:${PORT}/graphql for ${process.env.NODE_ENV}`);
-  await sequelize.authenticate();
-  await sequelize.sync({ force: true });
+  await db.init();
   logger.log('💾 Connection to database established successfully');
 });
 
--- src/config/db.js
+++ src/config/db.js
@@ -7,16 +7,23 @@ const { NODE_ENV } = process.env;
 
 const db = config[NODE_ENV];
 
-const init = () => {
+const init = async () => {
+  let sequelize;
   if (NODE_ENV === 'production') {
-    return new Sequelize(db.url, {
+    sequelize = new Sequelize(db.url, {
       dialect: db.dialect,
     });
+  } else {
+    sequelize = new Sequelize(db.database, db.username, db.password, {
+      dialect: db.dialect,
+      host: db.host,
+    });
   }
-  return new Sequelize(db.database, db.username, db.password, {
-    dialect: db.dialect,
-    host: db.host,
-  });
+  await sequelize.authenticate();
+  await sequelize.sync({ force: true });
+  return sequelize;
 };
 
-export default init();
+export default {
+  init,
+};
