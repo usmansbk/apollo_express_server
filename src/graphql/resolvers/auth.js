269d46253e7c11e8906db1d0c29a69db064eef04
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-16 11:25:34 +0100

    mock

--- src/graphql/resolvers/auth.js
+++ src/graphql/resolvers/auth.js
@@ -1,6 +1,16 @@
 export default {
   Mutation: {
-    signIn: () => null,
+    signIn: () => ({
+      code: '200',
+      success: true,
+      message: 'Login successful',
+      user: {
+        id: 1,
+        firstName: 'usman',
+        lastName: 'suleiman',
+      },
+      token: '1234',
+    }),
     signUp: () => null,
     socialLogin: () => null,
   },
