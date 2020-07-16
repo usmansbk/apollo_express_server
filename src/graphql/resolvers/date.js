6ded2dbef9f7c59957459a0c8a60fa845760a115
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-16 03:00:36 +0100

    add custom date type

---
+++ src/graphql/resolvers/date.js
@@ -0,0 +1,14 @@
+import { GraphQLScalarType } from 'graphql';
+
+export default {
+  Date: new GraphQLScalarType({
+    name: 'Date',
+    description: 'date as a string value in ISO format',
+    parseValue(value) {
+      return new Date(value);
+    },
+    serialize(value) {
+      return new Date(value).toISOString();
+    },
+  }),
+};
--- src/graphql/resolvers/user.js
+++ src/graphql/resolvers/user.js
@@ -1,6 +1,12 @@
 export default {
   Query: {
-    user: () => 'user',
+    user: () => ({
+      id: 1,
+      firstName: 'usman',
+      lastName: 'suleiman',
+      email: 'usmansbk@gmail.com',
+      createdAt: new Date(),
+    }),
     me: () => 'me',
   },
 };
