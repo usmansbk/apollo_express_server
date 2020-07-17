5332cf7263f91fc06bf774a2054d0853b61ab7e0
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-17 03:08:26 +0100

    chore

--- db/models/user.js
+++ db/models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    getFullname() {
+    fullName() {
       return [this.firstName, this.lastName].join(' ');
     }
     static associate(models) {
