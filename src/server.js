110df8970818af26c23e4fa32d886263aa69e912
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-18 04:41:12 +0100

    cookie parser

--- src/server.js
+++ src/server.js
@@ -1,7 +1,11 @@
 import express, { json, urlencoded } from 'express';
+import cookieParser from 'cookie-parser';
+
+require('dotenv').config();
 
 const app = express();
 
+app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(express.static('docs'));
