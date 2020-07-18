7186c77060ad7e6dc133c28aa3f1b76519ac9f69
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-18 03:51:47 +0100

    add session model

---
+++ db/migrations/20200718024843-create-session.js
@@ -0,0 +1,26 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Sessions', {
+      id: {
+        type: Sequelize.UUID,
+        primaryKey: true,
+        allowNull: false,
+      },
+      refreshToken: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Sessions');
+  }
+};
