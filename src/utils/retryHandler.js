1988c74e38abe4b0737e7036f21f05443a685cab
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-19 14:33:03 +0100

    chore

--- src/utils/retryHandler.js
+++ src/utils/retryHandler.js
@@ -4,12 +4,14 @@ import logger from '../config/logger';
 export default function faultTolerantResolve(cb) {
   const operation = retry.operation();
 
-  operation.attempt(async (currentAttempt) => {
-    try {
-      console.log('CURRENT', currentAttempt);
-      await cb();
-    } catch (error) {
+  operation.attempt((currentAttempt) => {
+    cb().catch((err) => {
+      if (operation.retry(err)) {
+        logger.log(`Attempt#${currentAttempt} - ${err.message}`);
+        logger.error(err);
+        return;
+      }
       logger.error(operation.mainError());
-    }
+    });
   });
 }
