7bb32c0836e215cf9e0bba34e548a0e66b56f2e2
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-23 00:50:06 +0100

    add controllers

--- src/graphql/resolvers/auth.js
+++ src/graphql/resolvers/auth.js
@@ -1,23 +1,33 @@
-import { Auth as Controller, SocialAuth } from '../../controllers';
+import { Auth, SocialAuth, Admin } from '../../controllers';
 
 export default {
   Mutation: {
-    login: Controller.login,
-    signUp: Controller.signUp,
+    login: Auth.login,
+    signUp: Auth.signUp,
     socialLogin: SocialAuth.socialLogin,
-    refreshToken: Controller.refreshToken,
-    updateEmail: Controller.updateEmail,
-    requestChangeEmail: Controller.requestChangeEmail,
-    updatePassword: Controller.updatePassword,
-    requestResetPassword: Controller.requestResetPassword,
-    updateProfile: Controller.updateProfile,
-    deleteAccount: Controller.deleteAccount,
-    requestDeleteAccount: Controller.requestDeleteAccount,
-    verifyEmailAddress: Controller.verifyEmailAddress,
-    resendEmailVerificationLink: Controller.resendEmailVerificationLink,
-    logout: Controller.logout,
+    refreshToken: Auth.refreshToken,
+    updateEmail: Auth.updateEmail,
+    requestChangeEmail: Auth.requestChangeEmail,
+    updatePassword: Auth.updatePassword,
+    requestResetPassword: Auth.requestResetPassword,
+    updateProfile: Auth.updateProfile,
+    deleteAccount: Auth.deleteAccount,
+    requestDeleteAccount: Auth.requestDeleteAccount,
+    verifyEmailAddress: Auth.verifyEmailAddress,
+    resendEmailVerificationLink: Auth.resendEmailVerificationLink,
+    logout: Auth.logout,
 
     // Admin mutations
-    sudo: Controller.sudo,
+    sudo: Auth.sudo,
+    createUser: Admin.createUser,
+    updateUser: Admin.updateUser,
+    deleteUser: Admin.deleteUser,
+    batchCreateUser: Admin.batchCreateUser,
+    batchUpdateUser: Admin.batchUpdateUser,
+    batchDeleteUser: Admin.batchDeleteUser,
+
+    // Owner mutations
+    makeAdmin: Admin.makeAdmin,
+    removeAdmin: Admin.removeAdmin,
   },
 };
