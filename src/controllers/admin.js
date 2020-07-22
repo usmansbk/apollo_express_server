2db9f1823f9f017f1ea201ca964413a1c8cf41d7
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-23 00:49:43 +0100

    stub

---
+++ src/controllers/admin.js
@@ -0,0 +1,25 @@
+export default class Admin {
+  static createUser() {
+  }
+
+  static updateUser() {
+  }
+
+  static deleteUser() {
+  }
+
+  static batchCreateUser() {
+  }
+
+  static batchUpdateUser() {
+  }
+
+  static batchDeleteUser() {
+  }
+
+  static makeAdmin() {
+  }
+
+  static removeAdmin() {
+  }
+}
