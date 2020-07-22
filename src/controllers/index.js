65e47cb864255ced66361b7feb43e7d8e1dc717e
Babakolo Usman Suleiman <usmansbk@gmail.com>
2020-07-23 00:49:51 +0100

    stub

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,11 +1,15 @@
 import Auth from './auth';
 import SocialAuth from './social_auth';
 import User from './user';
+import Admin from './admin';
 
-export { Auth, User, SocialAuth };
+export {
+  Auth, User, SocialAuth, Admin,
+};
 
 export default {
   Auth,
   User,
   SocialAuth,
+  Admin,
 };
